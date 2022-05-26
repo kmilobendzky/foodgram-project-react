384aa3bd66e14f8bcb1fbd28d4f95b9fa35e5ee2
kmilobendzky <k.milobendzky@yandex.ru>
2022-05-26 15:47:45 +0300

    Deploying again

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -176,8 +176,8 @@ DJOSER = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_URL = "/static_files/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static_files")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
